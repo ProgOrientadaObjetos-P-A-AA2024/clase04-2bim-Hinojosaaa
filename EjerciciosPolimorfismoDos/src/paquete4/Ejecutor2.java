2e9bdf12186bcd734be84766a8767042c3c6031b
Andres <adhinojosa2@utpl.edu.ec>
2024-06-27 13:25:36 -0500

    ejecutor2

--- EjerciciosPolimorfismoDos/src/paquete4/Ejecutor2.java
+++ EjerciciosPolimorfismoDos/src/paquete4/Ejecutor2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete4;
 
 import java.util.ArrayList;
@@ -10,42 +5,95 @@ import paquete2.Arriendo;
 import paquete3.ArriendoLocalComercial;
 import paquete3.ArriendoLocalComida;
 import paquete3.ArriendoLocalSesiones;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ejecutor2 {
+
     public static void main(String[] args) {
-        
-        
-        
-        ArrayList<Arriendo> listaArriendos = new ArrayList<>();
-        
-        ArriendoLocalComida arriendoComida = new ArriendoLocalComida(
-                "Christian Shepherd", 300);
-        arriendoComida.establecerIva(10); // en porcentaje
-        arriendoComida.establecerValorAgua(20.2); // en $
-        arriendoComida.establecerValorLuz(40.2); // en $
-        
-        ArriendoLocalComercial arriendoComercial = new ArriendoLocalComercial(
-                "Andrew Schroeder", 400);
-        arriendoComercial.establecerValorAdicionalFijo(100); // en $
-        
-        ArriendoLocalSesiones arriendoSesiones = new ArriendoLocalSesiones(
-                "Angela Watson", 350);
-        arriendoSesiones.establecerValorSillas(10); // en $
-        arriendoSesiones.establecerValorAmplificacion(20); // en $
-        
-        listaArriendos.add(arriendoComida);
-        listaArriendos.add(arriendoComercial);
-        listaArriendos.add(arriendoSesiones);
-        
-        for (int i = 0; i < listaArriendos.size(); i++) {
-            listaArriendos.get(i).establecerArriendoMensual();
+        Scanner entrada = new Scanner(System.in);
+        ArrayList<Arriendo> arriendos = new ArrayList<>();
+        String nombre;
+        double cuotaB;
+        double valorAdicionalFijo;
+        double valorLuz;
+        double valorAgua;
+        double iva;
+        double valorSillas;
+        double valorAmplificacion;
+        int contador = 0;
+        int instancias;
+        int opc;
+
+        System.out.println("Ingrese el número de locales que va a ingresar:");
+        instancias = entrada.nextInt();
+
+        while (contador < instancias) {
+            System.out.printf("[1] Arriendo Local Comercial\n"
+                    + "[2] Arriendo Local Comida\n"
+                    + "[3] Arriendo Local Sesiones\n");
+            opc = entrada.nextInt();
+            entrada.nextLine();
+
+            switch (opc) {
+                case 1:
+                    System.out.println("Ingrese el nombre del arrendatario:");
+                    nombre = entrada.nextLine();
+                    System.out.println("Ingrese la cuota base:");
+                    cuotaB = entrada.nextDouble();
+                    System.out.println("Ingrese el valor adicional fijo:");
+                    valorAdicionalFijo = entrada.nextDouble();
+
+                    ArriendoLocalComercial localComercial = new ArriendoLocalComercial(nombre, cuotaB);
+                    localComercial.establecerValorAdicionalFijo(valorAdicionalFijo);
+                    localComercial.establecerArriendoMensual();
+                    arriendos.add(localComercial);
+                    contador++;
+                    break;
+                case 2:
+                    System.out.println("Ingrese el nombre del arrendatario:");
+                    nombre = entrada.nextLine();
+                    System.out.println("Ingrese la cuota base:");
+                    cuotaB = entrada.nextDouble();
+                    System.out.println("Ingrese el valor de la luz:");
+                    valorLuz = entrada.nextDouble();
+                    System.out.println("Ingrese el valor del agua:");
+                    valorAgua = entrada.nextDouble();
+                    System.out.println("Ingrese el IVA:");
+                    iva = entrada.nextDouble();
+
+                    ArriendoLocalComida localComida = new ArriendoLocalComida(nombre, cuotaB, valorLuz, valorAgua, iva);
+                    localComida.establecerArriendoMensual();
+                    arriendos.add(localComida);
+
+                    contador++;
+                    break;
+                case 3:
+                    System.out.println("Ingrese el nombre del arrendatario:");
+                    nombre = entrada.nextLine();
+                    System.out.println("Ingrese la cuota base:");
+                    cuotaB = entrada.nextDouble();
+                    System.out.println("Ingrese el valor de las sillas:");
+                    valorSillas = entrada.nextDouble();
+                    System.out.println("Ingrese el valor de la amplificación:");
+                    valorAmplificacion = entrada.nextDouble();
+
+                    ArriendoLocalSesiones localSesiones = new ArriendoLocalSesiones(nombre, cuotaB);
+                    localSesiones.establecerValorSillas(valorSillas);
+                    localSesiones.establecerValorAmplificacion(valorAmplificacion);
+                    localSesiones.establecerArriendoMensual();
+                    arriendos.add(localSesiones);
+                    contador++;
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
         }
-        
-        CentroComercial centro = new CentroComercial("La Pradera", 
-                listaArriendos);
-        centro.establecerTotalArriendosBaseMensual();
-        centro.establecerTotalArriendosFinalMensual();
-        System.out.println(centro);
-    
+        entrada.close();
+
+        System.out.println("\nInformación de todos los locales:");
+        for (Arriendo arriendo : arriendos) {
+            System.out.println(arriendo);
+        }
     }
 }
